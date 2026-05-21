10e1bf79192ebe8ac10cba1969ec045fe185109c
Annette Samuelsen <annette.samuelsen@nersc.no>
2026-05-21 17:08:08 +0200

    New scripts (#1)

---
+++ SEACLIM/Transfer_to_edito.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+local_dir=/nird/datalake/NS9481K/shuang/seaclim/output
+edito_dir=edito/seaclim/reference_simulations/hindcast
+
+# BCG 3D-variables
+for vari in alk chl dic nh4 no3 o2 ph phyc po4 si zooc; do
+for year in {1993..2024}; do
+./Progs/mc cp ${local_dir}/3D/BGC/3DT-${vari}/TOPAZ2_1m-m_3DT-${vari}_${year}*.nc ${edito_dir}/ARC/BGC/Monthly/3DT-${vari}/
+done
+done
+
+# BGC 2D-variables
+for vari in cflx npp o2b phsurf zeu; do
+for year in {1993..2024}; do
+./Progs/mc cp ${local_dir}/2D/BGC/2DT-${vari}/TOPAZ2_1d-m_2DT-${vari}_${year}*.nc ${edito_dir}/ARC/BGC/Daily/2DT-${vari}/
+done
+done
+
+# PHY 3D-variables 
+for vari in so thetao uo vo wo; do
+for year in {1993..2024}; do
+./Progs/mc cp ${local_dir}/3D/PHY/3DT-${vari}/TOPAZ2_1m-m_3DT-${vari}_${year}*.nc ${edito_dir}/ARC/PHY/Monthly/3DT-${vari}/
+done
+done
+
+# PHY 2D-variables                                                                                                                                                                   
+for vari in mld sbs sbt sss sst; do
+for year in {1993..2024}; do
+./Progs/mc cp ${local_dir}/2D/PHY/2DT-${vari}/TOPAZ2_1d-m_2DT-${vari}_${year}*.nc ${edito_dir}/ARC/PHY/Daily/2DT-${vari}/
+done
+done
+
+# CICE 2D-variables
+for vari in aice hi; do
+for year in {1993..2024}; do
+./Progs/mc cp ${local_dir}/2D/cice/2DT-${vari}/TOPAZ2_1d-m_2DT-${vari}_${year}*.nc ${edito_dir}/ARC/SEAICE/Daily/2DT-${vari}/
+done
+done
---
+++ SEACLIM/Transport_1year.sh
@@ -0,0 +1,24 @@
+#!/bin/sh 
+
+year=$1
+
+pwd
+cp transport_atl.in Y${year}/transport.in
+cp sections.in Y${year}/
+cp section_intersect Y${year}/
+cp section_transport2 Y${year}/
+cp section_transport Y${year}/
+cp m2transport Y${year}/
+cp regional.* Y${year}/
+cp section*.dat Y${year}/
+cp transport*.dat Y${year}/
+cd Y${year}/
+
+./m2transport archm.${year}*.b
+
+for sec in {001..008}; do
+   mv tst${sec}.nc ../tst${sec}.nc 
+done
+mv transports.nc ../transports_${year}.nc
+
+cd ..
